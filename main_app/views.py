a2dd0f2b719321170e9a930820b9db43f050f7ab
Shane Dupree <shane.dupree@gmail.com>
2022-01-14 19:29:28 -0500

    Map update and API initialize.

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Add the following import
 from django.http import HttpResponse
+import requests
 
 # Define the home view
 def home(request):
+  r = requests.get('https://opensky-network.org/api/states/all?lamin=43&lomin=-74&lamax=44&lomax=-73')
+  print(r.text)  
   return render(request, 'home.html')
