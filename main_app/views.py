305a24065ab3855f5c7fd6534e5642c0801d0314
ttyngai <ttyngai@gmail.com>
2022-01-17 17:34:53 -0800

    Add plane location

--- main_app/views.py
+++ main_app/views.py
@@ -24,8 +24,15 @@ class Flight:
 
 # Define the home view
 def home(request):
+
+  # need to get request from that refresh
+  request.latmin = 43
+  request.latmax = 55
+  request.longmin = -75
+  request.longmax = -70
   # Fetch data and store in variable
-  flight_data = requests.get('https://opensky-network.org/api/states/all?lamin=40&lomin=-80&lamax=48&lomax=-70').json()
+
+  flight_data = requests.get(f'https://opensky-network.org/api/states/all?lamin={request.latmin}&lomin={request.longmin}&lamax={request.latmax}&lomax={request.longmax}').json()
   # Parsing data/Adding key to match sighting model
   sample_self_coordinates = {
     'long': -78,
@@ -48,7 +55,45 @@ def home(request):
   # closest 100 flights
   closest_flights = sort_flight_by_distance[:100]
   # print((closest_flights))
-  # for flight in closest_flights:
-  #   print(flight.callsign)
+  for idx, flight in enumerate(closest_flights):
+    print(idx, flight.distance_from_self)
+  # slice list to take top 100(closest) flights
+  return render(request, 'home.html', {'closest_flights': closest_flights})
+
+def home_update(request, latMax, latMin, longMax,longMin):
+  # need to get request from that refresh
+  latMin = latMin/10000000000000000
+  latMax = latMax/10000000000000000
+  longMin = -(longMin/10000000000000000)
+  longMax = -(longMax/10000000000000000)
+  # Fetch data and store in variable
+  flight_data = requests.get(f'https://opensky-network.org/api/states/all?lamin={latMin}&lomin={longMin}&lamax={latMax}&lomax={longMax}').json()
+  # Parsing data/Adding key to match sighting model
+  selfLong = (longMin + longMax)/2
+  selfLat = (latMin + latMax)/2
+  sample_self_coordinates = {
+    'lat': selfLat,
+    'long': selfLong,
+  }
+  flight_data_parsed=[]
+  if flight_data['states']:
+    for flight in flight_data['states']:
+      # Parsing data/including distance from self
+      long_delta = flight[5]-sample_self_coordinates['long']
+      lat_delta = flight[6]-sample_self_coordinates['lat']
+      distance_from_self = math.sqrt((long_delta*long_delta)+(lat_delta*lat_delta))
+
+      # NEED TO DO: Should check if all data(at least the important ones) are present, or if in the sky
+
+      obj = Flight(flight[1], flight[3], flight[5], flight[6], flight[7], flight[8], flight[9], flight[10], flight[11], distance_from_self)
+      flight_data_parsed.append(obj)
+  # Sort flight by distance from self
+  sort_flight_by_distance = sorted(flight_data_parsed, key=lambda flight: flight.distance_from_self)
+
+  # closest 100 flights
+  closest_flights = sort_flight_by_distance[:100]
+  # print((closest_flights))
+  for idx, flight in enumerate(closest_flights):
+    print(idx, flight.distance_from_self)
   # slice list to take top 100(closest) flights
   return render(request, 'home.html', {'closest_flights': closest_flights})
