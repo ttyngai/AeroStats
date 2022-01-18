a4520c7482430cd0b38777a2e634e0c1b99979b8
ttyngai <ttyngai@gmail.com>
2022-01-17 17:34:53 -0800

    Add plane location

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('update/<int:latMax>/<int:latMin>/<int:longMax>/<int:longMin>/', views.home_update, name='home_update'),
+
 ]
