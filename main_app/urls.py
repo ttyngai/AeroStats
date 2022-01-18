3ecf3bef2b4032c01a2ceadb541e15154018d592
ttyngai <ttyngai@gmail.com>
2022-01-17 20:20:07 -0800

    Fix negative URL pattern. Can see whole whole's flights

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('update/<int:latMax>/<int:latMin>/<int:longMax>/<int:longMin>/', views.home_update, name='home_update'),
+    path('update/<str:latMax>/<str:latMin>/<str:longMax>/<str:longMin>/', views.home_update, name='home_update'),
 
 ]
