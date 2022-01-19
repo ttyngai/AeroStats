f4fdf3c62ab1f208b3468482041a57ae086ce73d
Dyv Lee <d1j3l@Dyvs-MacBook-Pro.local>
2022-01-19 15:24:18 -0800

    add login and logout

--- main_app/views.py
+++ main_app/views.py
@@ -2,13 +2,15 @@ from fileinput import close
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from random import sample
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Plane
 from .forms import PlaneForm
 
 def home(request):
   watchlist = Plane.objects.all()
-  return render(request, 'home.html', { 'watchlist': watchlist })
+  login_form = AuthenticationForm()
+  return render(request, 'home.html', { 'watchlist': watchlist, 'login_form': login_form })
+  
 
 class PlaneCreate(CreateView):
   model = Plane
