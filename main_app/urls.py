e2c753b4e590ed9e8dc9d9a73f3c88598be625c8
Dyv Lee <d1j3l@Dyvs-MacBook-Pro.local>
2022-01-19 19:39:27 -0800

    Add delete to watchlist

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('planes/create/', views.add_plane, name='planes_create'),
+    path('planes/<int:plane_id>/', views.planes_detail, name='detail'),
     path('planes/<int:pk>/update/', views.PlaneUpdate.as_view(), name='planes_update'),
     path('planes/<int:pk>/delete/', views.PlaneDelete.as_view(), name='planes_delete'),
 ]
