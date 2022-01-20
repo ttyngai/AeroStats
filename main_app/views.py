90a1fdbfa517da3d5c87b0ce5782ddb099073e17
ttyngai <ttyngai@gmail.com>
2022-01-19 23:19:51 -0800

    Complete watchlist styling

--- main_app/views.py
+++ main_app/views.py
@@ -5,11 +5,53 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from .models import Plane
 from .forms import PlaneForm
+import requests
 
 def home(request):
-  watchlist = Plane.objects.all()
+  watch_db = Plane.objects.all()
+  flight_data = requests.get('https://opensky-network.org/api/states/all').json()
+  watchlist=[]
+  for flight in flight_data['states']:
+    # print(flight[0])
+    for plane in watch_db:
+      # print('db', plane.icao24)
+      if plane.icao24 == flight[0]:  
+        f = {
+        'icao24': flight[0],
+        'callsign': flight[1],
+        'origin_country': flight[2],
+        'longitude': flight[5],
+        'latitude': flight[6],
+        'altitude': flight[7],
+        'on_ground': flight[8],
+        'velocity': flight[9],
+        'true_track': flight[10],
+        'vertical_rate': flight[11]
+        }
+        watchlist.append(f)
+  for plane in watch_db:
+    not_online = True
+    for f in watchlist:
+      if plane.icao24 == f['icao24']:
+        not_online = False
+    if not_online:
+
+      f = {
+        'icao24': plane.icao24,
+        'callsign': 'n/a',
+        'origin_country': 'n/a',
+        'longitude': 'n/a',
+        'latitude': 'n/a',
+        'altitude': 'n/a',
+        'on_ground': 'n/a',
+        'velocity': 0,
+        'true_track': 'n/a',
+        'vertical_rate': 'n/a',
+        }
+      watchlist.append(f)
+
   login_form = AuthenticationForm()
-  return render(request, 'home.html', { 'watchlist': watchlist, 'login_form': login_form })
+  return render(request, 'home.html', { 'watchlist': watch_db, 'login_form': login_form ,'watchlist_populated': watchlist}, )
   
 
 class PlaneCreate(CreateView):
