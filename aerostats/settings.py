717a09c012edec891612e00a19df992181d1c62f
ttyngai <ttyngai@gmail.com>
2026-01-20 18:54:50 -0800

    Add AWS Elastic Beanstalk deployment configuration
    - Add .ebextensions config for Django
    - Update settings for production (env vars, ALLOWED_HOSTS)
    - Add whitenoise for static files
    - Simplify requirements.txt

--- aerostats/settings.py
+++ aerostats/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u@5_n_6wal@(gwj(f$b!!o&@nvj9pj)4_8r$pvnsu9*))r)@y8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-u@5_n_6wal@(gwj(f$b!!o&@nvj9pj)4_8r$pvnsu9*))r)@y8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS += ['.elasticbeanstalk.com', '.amazonaws.com']
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +124,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
